d6b8d486ff5541ef589780d5862795b9a9f91f0d
MAKARnikanov <maka.nik.00@mail.ru>
2023-03-24 13:13:16 +0300

    Электротовары дз 1 готовы

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
+import utils
 
+item1 = utils.Item("Смартфон", 10000, 20)
+item2 = utils.Item("Ноутбук", 20000, 5)
+
+print(item1.calculate_total_price())
+print(item2.calculate_total_price())
+
+utils.Item.pay_rate = 0.8
+item1.apply_discount()
+print(item1.price)
+print(item2.price)
+
+print(utils.Item.all)
--- utils.py
+++ utils.py
@@ -9,8 +9,11 @@ class Item:
         self.all += (self, name, price, count)
 
     def returned(self):
-        desc = str(self.name) + " " + self.price + self.quant
+        desc = str(self.name) + " " + self.price + self.count
         return desc.title()
 
     def calculate_total_price(self):
         self.total_price = self.price * self.count * self.pay_rate
+        return self.total_price
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
